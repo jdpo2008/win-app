6a70cff57b699c9b2202a2395521188be32adb74
Jose Daniel <jdpo2008@gmail.com>
2022-10-14 12:43:12 -0500

    Primer commit de la aplicacion

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import NavbarStyle from "@components/_App/NavbarStyle";
+import MainBanner from "@components/Home/MainBanner";
+
+export default function Home() {
+  return (
+    <>
+      <NavbarStyle />
+
+      <MainBanner />
+    </>
+  );
+}
