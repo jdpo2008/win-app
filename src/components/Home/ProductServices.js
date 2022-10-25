44d2e2e8b725a1f271f7fa10cc7fbdee65d94d74
Jose Daniel <jdpo2008@gmail.com>
2022-10-25 13:16:48 -0500

    JDP: se continua con la seccion de servicios

--- src/components/Home/ProductServices.js
+++ src/components/Home/ProductServices.js
@@ -1,11 +1,15 @@
 import * as React from "react";
 import Container from "@mui/material/Container";
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
 
 import StyledTabs from "./styled/StyledTabs";
 import StyledTab from "./styled/StyledTab";
 import StyledTabPanel from "./styled/StyledTabPanel";
 
 import { Products } from "../../data/products";
+
+import { Services } from "../../data/services";
+import { spacing } from "@mui/system";
 
 const ProductServices = () => {
   const [value, setValue] = React.useState(0);
@@ -15,6 +19,8 @@ const ProductServices = () => {
   };
 
   console.log(Products);
+
+  console.log(Services);
 
   return (
     <Container
@@ -42,13 +48,72 @@ const ProductServices = () => {
         <StyledTab className="product-area-tabs-item" label="" />
       </StyledTabs>
       <div className="product-area-tabs-panel">
-        {Products.map((product, i) => {
-          return (
-            <StyledTabPanel value={value} index={i} key={i}>
-              Item {i}
-            </StyledTabPanel>
-          );
-        })}
+        {Products &&
+          Products.map((product, i) => {
+            return (
+              <StyledTabPanel value={value} index={i} key={i}>
+                {Services &&
+                  Services.filter((s) => s.productId === product.id).map(
+                    (service, i) => {
+                      return (
+                        <div>
+                          <div
+                            className="product-area-services-container"
+                            key={i}
+                          >
+                            {service.has_promotion && (
+                              <div className="product-area-services-container-promotion">
+                                <span>X2</span>
+                              </div>
+                            )}
+                            <div className="product-area-services-card">
+                              <h5 className="product-area-services-card-nombre">
+                                {service.nombre}
+                              </h5>
+                              <div className="product-area-services-card-logo"></div>
+                              {service.velocidad_anterior && (
+                                <span className="product-area-services-card-velocidad-antigua">
+                                  {service.velocidad_anterior}
+                                </span>
+                              )}
+                              {!service.velocidad_anterior && (
+                                <span className="product-area-services-card-velocidad-antigua">
+                                  &nbsp;
+                                </span>
+                              )}
+                              <h4 className="product-area-services-card-velocidad">
+                                {service.velocidad_actual}{" "}
+                                <span>{service.promotion}</span>
+                              </h4>
+                              <p className="product-area-services-card-precio">
+                                Precio S/{" "}
+                                {service.precio && (
+                                  <span>{service.precio}</span>
+                                )}{" "}
+                                al mes
+                              </p>
+                              {service.features &&
+                                service.features.map((f) => (
+                                  <div
+                                    key={f.id}
+                                    className="product-area-services-card-features"
+                                  >
+                                    {f.descripcion}
+                                  </div>
+                                ))}
+                              <button className="product-area-services-card-button">
+                                ¡Pidelo por WhatsApp{" "}
+                                <WhatsAppIcon className="product-area-services-card-button-icon" />
+                              </button>
+                            </div>
+                          </div>
+                        </div>
+                      );
+                    }
+                  )}
+              </StyledTabPanel>
+            );
+          })}
       </div>
     </Container>
   );
--- src/components/Home/styled/StyledTabPanel.js
+++ src/components/Home/styled/StyledTabPanel.js
@@ -1,6 +1,4 @@
 import * as React from "react";
-import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
 
 const StyledTabPanel = (props) => {
   const { children, value, index, ...other } = props;
@@ -14,9 +12,15 @@ const StyledTabPanel = (props) => {
       {...other}
     >
       {value === index && (
-        <Box>
-          <Typography>{children}</Typography>
-        </Box>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            width: "cal(100vw)",
+          }}
+        >
+          {children}
+        </div>
       )}
     </div>
   );
