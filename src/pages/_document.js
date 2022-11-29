d1f4256861e05c9cdab60e30408ef2d12a119bf0
Jose Daniel <jdpo2008@gmail.com>
2022-11-29 08:31:50 -0500

    JDP: se agrega soporte SEO

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,5 +1,7 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 import React from "react";
+// theme
+import palette from "../theme/palette";
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -23,7 +25,45 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <meta charSet="utf-8" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/favicon/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon/favicon-16x16.png"
+          />
+
+          <meta name="theme-color" content={palette.light.primary.main} />
+          <link rel="manifest" href="/manifest.json" />
+
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Public+Sans:wght@400;500;600;700&display=swap"
+            rel="stylesheet"
+          />
+
+          <meta
+            name="description"
+            content="The starting point for your next project with Win-App"
+          />
+          <meta
+            name="keywords"
+            content="nextjs,win,internet,react,kit,application"
+          />
+          <meta name="author" content="innovacionesjd.com" />
+        </Head>
         <body>
           <Main />
           <NextScript />
