3d04e6336dd963836f4e456105bb28c941a6683a
Jose Daniel <jdpo2008@gmail.com>
2022-11-29 09:49:46 -0500

    JDP: se agrega icon

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -25,45 +25,7 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head>
-          <meta charSet="utf-8" />
-          <link
-            rel="apple-touch-icon"
-            sizes="180x180"
-            href="/favicon/apple-touch-icon.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="/favicon/favicon-32x32.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="/favicon/favicon-16x16.png"
-          />
-
-          <meta name="theme-color" content={palette.light.primary.main} />
-          <link rel="manifest" href="/manifest.json" />
-
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Public+Sans:wght@400;500;600;700&display=swap"
-            rel="stylesheet"
-          />
-
-          <meta
-            name="description"
-            content="The starting point for your next project with Win-App"
-          />
-          <meta
-            name="keywords"
-            content="nextjs,win,internet,react,kit,application"
-          />
-          <meta name="author" content="innovacionesjd.com" />
-        </Head>
+        <Head />
         <body>
           <Main />
           <NextScript />
