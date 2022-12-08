71cffeed1559aa158482017ffb13b5429d1a1199
Jose Daniel <jdpo2008@gmail.com>
2022-12-08 11:59:59 -0500

    JDP: optimizacion de SEO

--- src/components/_App/Page.tsx
+++ src/components/_App/Page.tsx
@@ -27,53 +27,32 @@ const Page: FC<Props> = forwardRef(
     ref
   ) => (
     <>
-      <title key="title">{`${title} | WIN-APP`}</title>
-      <meta name="description" content={description} />
-      <meta key="og_type" property="og:type" content="website" />
-      <meta key="og_title" property="og:title" content={title} />
-      <meta
-        key="og_description"
-        property="og:description"
-        content={description}
-      />
-      <meta key="og_locale" property="og:locale" content="es_PE" />
-      <meta key="og_site_name" property="og:site_name" content="WIN-APP" />
-      <meta key="og_url" property="og:url" content={DOMAIN + `${url}`} />
-      <meta key="og_site_name" property="og:site_name" content="WIN-APP" />
-      <meta key="og_image" property="og:image" content={DEFAULT_OG_IMAGE} />
-      <meta
-        key="og_image:alt"
-        property="og:image:alt"
-        content={`${title} | WIN-APP`}
-      />
-      <meta key="og_image:width" property="og:image:width" content="1200" />
-      <meta key="og_image:height" property="og:image:height" content="630" />
-
-      <meta name="robots" content="index,follow" />
-
-      <meta
-        key="twitter:card"
-        name="twitter:card"
-        content="summary_large_image"
-      />
-      <meta key="twitter:title" property="twitter:title" content={title} />
-      <meta
-        key="twitter:description"
-        property="twitter:description"
-        content={description}
-      />
-
-      <link rel="canonical" href={DOMAIN} />
-
-      {/* <NextSeo
+      <NextSeo
         title={`${title} | WIN-APP`}
         description={description}
+        canonical={DOMAIN}
         openGraph={{
-          url: `httpa://cambiateawin.pe${url}`,
-          title,
-          description,
+          url: `https://cambiateawin.pe${url}`,
+          title: `${title}`,
+          description: `${description}`,
+          siteName: "WIN-APP",
+          images: [
+            {
+              url: DEFAULT_OG_IMAGE,
+              width: 800,
+              height: 600,
+              alt: "Og Image Alt",
+              type: "image/jpeg",
+            },
+          ],
         }}
-      /> */}
+        // twitter={{
+        //   handle: "@handle",
+        //   site: "@site",
+        //   cardType: "summary_large_image",
+        // }}
+        // facebook={{}}
+      />
 
       <Box ref={ref} {...other}>
         {children}
